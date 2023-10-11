c2e112c8954b3c1b1d11a4b1f88671ffd1efc764
Kevin <kevin.paez1010@gmail.com>
2023-10-11 16:23:48 -0600

    [refactor] - images added to public

--- src/data.ts
+++ src/data.ts
@@ -2,13 +2,13 @@ export const testimonials = [
   {
     name: "Laura Gonzáles",
     testimony: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    image: "./src/assets/images/laura-gonzales.webp"
+    image: "./images/laura-gonzales.webp"
 
   },
   {
     name: "Pedro Ramírez",
     testimony: " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    image: "./src/assets/images/pedro-ramirez.webp"
+    image: "./images/pedro-ramirez.webp"
 
   }
 ]
@@ -16,15 +16,15 @@ export const testimonials = [
 export const talks = [
   {
     name: "FINANZAS",
-    image: "./src/assets/images/finanzas.webp",
+    image: "./images/finanzas.webp",
   },
   {
     name: "EMPRENDIMIENTO",
-    image: "./src/assets/images/emprendimiento.webp",
+    image: "./images/emprendimiento.webp",
   },
   {
     name: "CRECIMIENTO PERSONAL",
-    image: "./src/assets/images/crecimiento-personal.webp",
+    image: "./images/crecimiento-personal.webp",
   }
 
 ]
@@ -34,28 +34,28 @@ export const articles = [
     id: "1",
     title: "Impulsarán a emprendedores en Expo Independencia económica",
     text: "La tercera edición de Expo Independencia Económica (EIE) se acerca, y con ella, la oportunidad de descubrir los secretos que te llevarán a un buen camino en materia de finanzas personales.",
-    image: "./src/assets/images/mano-sosteniendo-moneda-mexicana.webp",
+    image: "./images/mano-sosteniendo-moneda-mexicana.webp",
     link: "https://www.eltiempo.com/empresas/impulsar-a-emprendedores-en-expo-independencia-economico-20211127",
   },
   {
     id: "2",
     title: "Evolucionar a mentalidad triunfadora, el llamado a participar en la EIE",
     text: "Evolucionar a mentalidad triunfadora, el llamado a participar en la EIE.",
-    image: "./src/assets/images/crecimiento-mi-empresa-es-impresionante.webp",
+    image: "./images/crecimiento-mi-empresa-es-impresionante.webp",
     link: "https://www.eltiempo.com/empresas/evolucionar-a-mentalidad-triunfadora-el-llamado-a-participar-en-la-eie-20211127",
   },
   {
     id: "3",
     title: "¿Quieres tener independencia económica?",
     text: "Estas preguntas y muchas más podrán ser resueltas en la próxima edición de la Expo Independencia Económica el 9 de noviembre en el Auditorio Telmex.",
-    image: "./src/assets/images/mano-sosteniendo-moneda-mexicana.webp",
+    image: "./images/mano-sosteniendo-moneda-mexicana.webp",
     link: "https://www.eltiempo.com/empresas/evolucionar-a-mentalidad-triunfadora-el-llamado-a-participar-en-la-eie-20211127",
   },
   {
     id: "4",
     title: "Presentan la 3era edición de Expo independencia económica",
     text: "¡México es un país multimillonario! speakers EIE.",
-    image: "./src/assets/images/crecimiento-mi-empresa-es-impresionante.webp",
+    image: "./images/crecimiento-mi-empresa-es-impresionante.webp",
     link: "https://www.eltiempo.com/empresas/evolucionar-a-mentalidad-triunfadora-el-llamado-a-participar-en-la-eie-20211127",
   }
 ]
@@ -63,37 +63,37 @@ export const articles = [
 export const galleryEvents = [
   {
     id: "1",
-    image: "./src/assets/images/gallery/event-gallery-1.png",
+    image: "./images/gallery/event-gallery-1.png",
     name: "Fotografía de edición 2",
   },
   {
     id: "2",
-    image: "./src/assets/images/gallery/event-gallery-2.png",
+    image: "./images/gallery/event-gallery-2.png",
     name: "Fotografía de edición 2",
   },
   {
     id: "3",
-    image: "./src/assets/images/gallery/event-gallery-3.png",
+    image: "./images/gallery/event-gallery-3.png",
     name: "Fotografía de edición 2",
   },
   {
     id: "4",
-    image: "./src/assets/images/gallery/event-gallery-4.png",
+    image: "./images/gallery/event-gallery-4.png",
     name: "Fotografía de edición 2",
   },
   {
     id: "5",
-    image: "./src/assets/images/gallery/event-gallery-5.png",
+    image: "./images/gallery/event-gallery-5.png",
     name: "Fotografía de edición 2",
   },
   {
     id: "6",
-    image: "./src/assets/images/gallery/event-gallery-6.png",
+    image: "./images/gallery/event-gallery-6.png",
     name: "Fotografía de edición 2",
   },
   {
     id: "7",
-    image: "./src/assets/images/gallery/event-gallery-7.png",
+    image: "./images/gallery/event-gallery-7.png",
     name: "Fotografía de edición 2",
   }
 ]
@@ -101,37 +101,37 @@ export const galleryEvents = [
 export const galleryAbout = [
   {
     id: "1",
-    image: "./src/assets/images/gallery/about-gallery-1.png",
+    image: "./images/gallery/about-gallery-1.png",
     name: "Fotografía de edición 1",
   },
   {
     id: "2",
-    image: "./src/assets/images/gallery/about-gallery-2.png",
+    image: "./images/gallery/about-gallery-2.png",
     name: "Fotografía de edición 1",
   },
   {
     id: "3",
-    image: "./src/assets/images/gallery/about-gallery-3.png",
+    image: "./images/gallery/about-gallery-3.png",
     name: "Fotografía de edición 1",
   },
   {
     id: "4",
-    image: "./src/assets/images/gallery/about-gallery-4.png",
+    image: "./images/gallery/about-gallery-4.png",
     name: "Fotografía de edición 1",
   },
   {
     id: "5",
-    image: "./src/assets/images/gallery/about-gallery-5.png",
+    image: "./images/gallery/about-gallery-5.png",
     name: "Fotografía de edición 1",
   },
   {
     id: "6",
-    image: "./src/assets/images/gallery/about-gallery-6.png",
+    image: "./images/gallery/about-gallery-6.png",
     name: "Fotografía de edición 1",
   },
   {
     id: "7",
-    image: "./src/assets/images/gallery/about-gallery-7.png",
+    image: "./images/gallery/about-gallery-7.png",
     name: "Fotografía de edición 1",
   }
 ]
@@ -139,37 +139,37 @@ export const galleryAbout = [
 export const iconsAbout = [
   {
     id: 1,
-    image: "./src/assets/images/about/execelente-icon.webp",
+    image: "./images/about/execelente-icon.webp",
     name: "Excelente"
   },
   {
     id: 2,
-    image: "./src/assets/images/about/social-icon.webp",
+    image: "./images/about/social-icon.webp",
     name: "Responsabilidad socila"
   },
   {
     id: 3,
-    image: "./src/assets/images/about/integridad-icon.webp",
+    image: "./images/about/integridad-icon.webp",
     name: "Integridad"
   },
   {
     id: 4,
-    image: "./src/assets/images/about/colaboracion-icon.webp",
+    image: "./images/about/colaboracion-icon.webp",
     name: "Colaboración"
   },
   {
     id: 5,
-    image: "./src/assets/images/about/compromiso-icon.webp",
+    image: "./images/about/compromiso-icon.webp",
     name: "Compromiso"
   },
   {
     id: 6,
-    image: "./src/assets/images/about/superacion-icon.webp",
+    image: "./images/about/superacion-icon.webp",
     name: "Superación"
   },
   {
     id: 7,
-    image: "./src/assets/images/about/libertad-icon.webp",
+    image: "./images/about/libertad-icon.webp",
     name: "Superación"
   }
 ]
@@ -178,49 +178,49 @@ export const speakers3 = [
   {
     id: 1,
     name: "Oso Trava",
-    image: "./src/assets/images/speakers/oso-trava.webp",
+    image: "./images/speakers/oso-trava.webp",
     description: "Conferencista y emprendedor serial, hoy en día CEO de 4 empresas. Creador de “Cracks Podcast” con más de 20 millones de descargas y más de 200 entrevistas a las mentes más brillantes en negocios, deportes y tecnología. Tiburón de Shark Tank México."
   },
   {
     id: 2,
     name: "Arturo Elias",
-    image: "./src/assets/images/speakers/arturo-elias.webp",
+    image: "./images/speakers/arturo-elias.webp",
     description: "El mejor negociador de LATAM. Director de alianzas estratégicas y Contenidos de Américo Móvil. Director de Fundación Telmex. Director de Uno TV y tiburón de Shark Tank México."
   },
   {
     id: 3,
     name: "Marco Antonio Regil",
-    image: "./src/assets/images/speakers/marco-antonio-regil.webp",
+    image: "./images/speakers/marco-antonio-regil.webp",
     description: "Exitoso animador de TV con 37 años de trayectoria. Conferencista y experto en desarrollo personal que ha ayudado a millones de personas en EEUU, México y toda América Latina."
   },
   {
     id: 4,
     name: "Adriana Gallardo",
-    image: "./src/assets/images/speakers/adriana-gallardo.webp",
+    image: "./images/speakers/adriana-gallardo.webp",
     description: "Considerada por Forbes México como una de las 100 mujeres más poderosas, CEO del emporio latino AGI Business Group, Conferencista Internacional, autora Best-Seller, y Tiburona de Shark Tank México."
   },
   {
     id: 5,
     name: "Paco Quevedo",
-    image: "./src/assets/images/speakers/paco-quevedo.webp",
+    image: "./images/speakers/paco-quevedo.webp",
     description: "Con más de 21 años de experiencia como contador fiscalista, especialista en impuestos. Destacado empresario: craedor del tequila El Financiero, asesor de negocios, conferencista y estratega comercial."
   },
   {
     id: 6,
     name: "Sergio Caballero",
-    image: "./src/assets/images/speakers/sergio-caballero.webp",
+    image: "./images/speakers/sergio-caballero.webp",
     description: "Fundador y Director de Expo Independencia Económica. Emprendedor apasionado de múltiples proyectos que busca alcanzar la prosperidad y la independencia económica e inspirar e impulsar a los demás en el camino."
   },
   {
     id: 7,
     name: "Karen Scarpetta",
-    image: "./src/assets/images/speakers/karen-scarpetta.webp",
+    image: "./images/speakers/karen-scarpetta.webp",
     description: "Karen Scarpetta es colombiana, profesional en Negocios Internacionales y actualmente lidera las operaciones de WeWork en la región de habla hispana de Latinoamérica; que actualmente incluye los mercados de México, Argentina, Chile y Colombia."
   },
   {
     id: 8,
     name: "Hugo Blum",
-    image: "./src/assets/images/speakers/hugo-blum.webp",
+    image: "./images/speakers/hugo-blum.webp",
     description: "Hugo Blum es un experimentado profesional en negocios y finanzas. Actualmente, se desempeña como CEO de 100 Ladrillos y es Co-Fundador y miembro del consejo de administración de yotepresto.com También es socio de KF Ventures."
   }
 ]
@@ -229,25 +229,25 @@ export const speakers2 = [
   {
     id: 1,
     name: "Marcus Dantus",
-    image: "./src/assets/images/speakers/marcus-dantus.webp",
+    image: "./images/speakers/marcus-dantus.webp",
     description: "Fundador y CEO de Starup México y Tiburón en Shark Tank México."
   },
   {
     id: 2,
     name: "Ana Habib",
-    image: "./src/assets/images/speakers/ana-habib.webp",
+    image: "./images/speakers/ana-habib.webp",
     description: "Consultora internacional Especialista en implementación de negocios con tecnología disrruptiva e innovación."
   },
   {
     id: 3,
     name: "Eduardo Robles",
-    image: "./src/assets/images/speakers/eduardo-robles.webp",
+    image: "./images/speakers/eduardo-robles.webp",
     description: "10x Coach de Ventas, Marketing y Negocios. Fundador de la Universidad de Emprendimiento."
   },
   {
     id: 4,
     name: "Paco Quevedo",
-    image: "./src/assets/images/speakers/paco-quevedo.webp",
+    image: "./images/speakers/paco-quevedo.webp",
     description: "Con más de 21 años de experiencia como contador fiscalista, especialista en impuestos. Destacado empresario: craedor del tequila El Financiero, asesor de negocios, conferencista y estratega comercial."
   }
 ]
@@ -256,13 +256,13 @@ export const speakers2Section2 = [
   {
     id: 5,
     name: "AltaGracia Gomez",
-    image: "./src/assets/images/speakers/altagracia-gomez.webp",
+    image: "./images/speakers/altagracia-gomez.webp",
     description: ""
   },
   {
     id: 6,
     name: "Hector Romero",
-    image: "./src/assets/images/speakers/hector-romero.webp",
+    image: "./images/speakers/hector-romero.webp",
     description: "Empresario e Inversionista en busca de un México Mejor."
   }
 ]
@@ -271,31 +271,31 @@ export const speakers1 = [
   {
     id: 1,
     name: "Marcus Dantus",
-    image: "./src/assets/images/speakers/marcus-dantus.webp",
+    image: "./images/speakers/marcus-dantus.webp",
     description: "Fundador y CEO de Starup México y Tiburón en Shark Tank México."
   },
   {
     id: 2,
     name: "Marisa Lazo",
-    image: "./src/assets/images/speakers/marisa-lazo.webp",
+    image: "./images/speakers/marisa-lazo.webp",
     description: "Fundadora y Directora en Pastelerías Marisa; Presidenta Consejo ITESM Campus GDA y Tiburuna de Shark Tank México."
   },
   {
     id: 3,
     name: "Erich Cisneros",
-    image: "./src/assets/images/speakers/erich-cisneros.webp",
+    image: "./images/speakers/erich-cisneros.webp",
     description: "Head of Supply, Field Operations and Market Management for Mexico, Caribbean and Central America for Airbnb."
   },
   {
     id: 4,
     name: "Alex Berman",
-    image: "./src/assets/images/speakers/alex-berman.webp",
+    image: "./images/speakers/alex-berman.webp",
     description: "Vicepresidente de Marketing de La Haus, la startup de bienes raíces líder en México y Colombia."
   },
   {
     id: 5,
     name: "Etienee Luquet",
-    image: "./src/assets/images/speakers/etienne-luquet.webp",
+    image: "./images/speakers/etienne-luquet.webp",
     description: "Director de Asuntos Regulatorios y Política Pública en Bitso México."
   },
 ]
